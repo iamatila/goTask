27c97e8e367156a27abd79dd142810978dba1d52
Atilola Oluwatobiloba <tobiatilola@gmail.com>
2023-12-19 22:39:54 +0100

    readme

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 			return err
 		}
 
-		// Save the file to the 'images' directory
 		err = c.SaveFile(file, filepath.Join("./images", file.Filename))
 		if err != nil {
 			return err
@@ -31,24 +30,22 @@ func main() {
 	app.Get("/download/:filename", func(c *fiber.Ctx) error {
 		filename := c.Params("filename")
 
-		// Open the file
 		file, err := os.Open(filepath.Join("./images", filename))
 		if err != nil {
 			return err
 		}
 		defer file.Close()
 
-		// Create the 'downloads' directory if it doesn't exist
+		// os.FileInfo("./downloads", os.ModePerm)
 		os.MkdirAll("./downloads", os.ModePerm)
 
-		// Create a new file in the 'downloads' directory
 		out, err := os.Create(filepath.Join("./downloads", filename))
 		if err != nil {
 			return err
 		}
 		defer out.Close()
 
-		// Copy the file
+		// ok, err = io.Copy(out, filename)
 		_, err = io.Copy(out, file)
 		if err != nil {
 			return err
